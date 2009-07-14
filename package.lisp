a9d8be67d2bf4bddea0b0a651758c1be1919315c
AJ Rossini <blindglobe@gmail.com>
2009-07-14 16:00:40 +0200

    export methods so we can simply include this.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- package.lisp
+++ package.lisp
@@ -2,4 +2,8 @@
 
 (defpackage #:xarray
   (:use :common-lisp :iterate :bind :ffa :cl-utilities)
-  (:shadowing-import-from :iterate :collecting :collect))
+  (:shadowing-import-from :iterate :collecting :collect)
+  (:export xref xtype xdims xdims* xdim xrank xsize xref-writeable-p take xsetf 
+	   ;; export conditions as well?
+
+	   ))
