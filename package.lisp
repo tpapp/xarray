2c8b7d24469fe2f97fcb1a901399efcf3755a5b9
AJ Rossini <blindglobe@gmail.com>
2009-07-22 08:38:13 +0200

    unittests allowed me to realize that we are lacking a few exports.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- package.lisp
+++ package.lisp
@@ -4,6 +4,12 @@
   (:use :common-lisp :iterate :bind :ffa :cl-utilities)
   (:shadowing-import-from :iterate :collecting :collect)
   (:export xref xtype xdims xdims* xdim xrank xsize xref-writeable-p take xsetf 
-	   ;; export conditions as well?
+	   
+	   slice slice-view
 
+	   take
+
+	   carray carray* cvector cvector*
+
+	   ;; export conditions as well?
 	   ))
