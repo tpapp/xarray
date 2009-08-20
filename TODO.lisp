1b56531407cc38687f16ac87773a064bad1995ac
AJ Rossini <blindglobe@gmail.com>
2009-08-20 08:06:30 +0200

    docs on how we might change the approach taken -- needs to be discussed and confirmed with Tamas.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- TODO.lisp
+++ TODO.lisp
@@ -4,6 +4,20 @@
 ;;(asdf:oos 'asdf:load-op 'xarray-test)
 
 
+;; Tamas was thinking about this being a general interface, but then
+;; included some specialized issues that probably should be considered
+;; here but handled elsewhere.  
+;;
+;; What I (Tony) am currently thinking about is to pay a penalty
+;; initially on speed of access and write a general interface using a
+;; range of possible back-ends.  So that we can get the interface
+;; clean:   xref pulls out a value and puts it int an array of the
+;; same structure, xref* pulls out a value and sticks it into a lisp
+;; array or scalar and returns it.   Speed can be handled later by
+;; doing a compile-time/run-time tradeoff, we will pay the
+;; compile-time penalty, in exchange for run-time advantages.  This
+;; fits into the theme of rapid prototyping (slow exec) with rapid
+;; execution (post-proto...).
 
 ;;; Checking current test state
 
