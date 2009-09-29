1353b885eb77282ce1535670cd34509a2dfb2a08
tpapp <tkpapp@gmail.com>
2009-09-29 11:39:19 +0200

    Further interface cleanup:
    - removed xref-writable-p; setting a read-only element should just
    signal an appropriate condition
    
    - sanitized interface for take: result type is determined by the
      keyword :as (replacing :result-type), further keywords can be used
      (eg for element type) depending on this, and :as nil (the default)
      gives an array.
    
    - reorganized files.

---
+++ src/operations.lisp
@@ -0,0 +1,76 @@
+(in-package :xarray)
+
+;;;;  Basic operations for objects with an xarray interface.
+;;;;
+;;;;  Bivariate numerical operations are defined on two _conformable_
+;;;;  objects (ie objects having the same xdim), or an object and the
+;;;;  scalar.  Sophisticated recycling rules (eg R's vector recycling)
+;;;;  are NOT supported, you have to be explicit and use a view for
+;;;;  that.  The reason for this is that these things look clever
+;;;;  initially, but in the long run they are just another obscure
+;;;;  source of bugs.
+;;;;
+;;;;  Operations cannot guess the element type of the result, so an
+;;;;  optional argument (with a reasonable default, if possible) is
+;;;;  available for that purpose.  The result type does not have to be
+;;;   a Lisp type, if you are producing something else.  The default
+;;;   methods return Lisp arrays.
+
+(defun xdim= (a b)
+  "Return non-nil iff the dimensions of A and B are the same."
+  (equal (xdims a) (xdims b)))
+
+(defmacro define-elementwise-operation (operation &optional (name (make-symbol* "X")))
+  `(defgeneric ,name (a b &optional result-type)
+     (:documentation "The elementwise sum of two conformable arrays, or an array and a scalar.")
+     (:method (a b &optional (result-type 'number))
+       ;; default: both A and B are arrays
+       (assert (xdims= a b))
+       (let* ((dims (xdims a))
+	      (result (make-array dims :element-type result-type))
+	      (dims-vec (coerce dims 'fixnum-vector)))
+	 (dotimes (i (xsize a))
+	   (let ((subscripts (rm-subscripts dims-vec i)))
+	     (setf (row-major-aref result i)
+		   (,operation (apply #'xref a subscripts) (apply #'xref b subscripts)))))
+	 result))
+     (:method (a (b number) &optional (result-type 'number))
+       ;; B is a scalar
+       (let* ((dims (xdims a))
+	      (result (make-array dims :element-type result-type))
+	      (dims-vec (coerce dims 'fixnum-vector)))
+	 (dotimes (i (xsize a))
+	   (let ((subscripts (rm-subscripts dims-vec i)))
+	     (setf (row-major-aref result i)
+		   (,operation (apply #'xref a subscripts) b))))
+	 result))
+     (:method ((a number) b &optional (result-type 'number))
+       ;; A is a scalar (I wish I could just call the method with B
+       ;; and A, but would not work for - and /).
+       (let* ((dims (xdims b))
+	      (result (make-array dims :element-type result-type))
+	      (dims-vec (coerce dims 'vector)))
+	 (dotimes (i (xsize b))
+	   (let ((subscripts (rm-subscripts dims-vec i)))
+	     (setf (row-major-aref result i)
+		   (,operation a (apply #'xref b subscripts)))))
+	 result))))
+
+(define-elementwise-operation +)
+(define-elementwise-operation -)
+(define-elementwise-operation *)
+(define-elementwise-operation /)
+
+(defgeneric x= (a b &optional eps)
+  (:documentation "Return non-nil if A and B have the same
+dimensions,, and the sup|A-B| <= eps.")
+  (:method (a b &optional (eps #.(sqrt single-float-epsilon)))
+    (assert (xdim= a b))
+    (let ((dims (xdims a)))
+      (dotimes (i (reduce #'* dims))
+	(let ((subscripts (cm-subscripts dims i)))
+	  (unless (<= (abs (- (apply #'xref a subscripts)
+			      (apply #'xref b subscripts)))
+		      eps)
+	    (return-from x= nil)))))
+    t))
